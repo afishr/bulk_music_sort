f58742befb78dc90a4634ce56ff32c0d8da8eb02
Alex Calugari <cross9986@gmail.com>
2017-08-01 00:29:37 +0300

    Edit building

---
+++ cmake-build-release/CMakeFiles/bulk_music_sort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bulk_music_sort.dir/main.cpp.obj"
+  "bulk_music_sort.pdb"
+  "bulk_music_sort.exe"
+  "bulk_music_sort.exe.manifest"
+  "libbulk_music_sort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bulk_music_sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
